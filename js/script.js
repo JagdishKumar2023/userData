c38f99642ba4c5fd0a3eafef2ef16842071d5d7f
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-11-10 22:29:00 +0530

    New Chanegs

--- js/script.js
+++ js/script.js
@@ -1,49 +1,79 @@
 // alert("hi");
 import { user_data } from "./user_data.js";
 
-function sortAge(a, b) {
-  if (a.age > b.age) {
-    return 1;
-  } else if (a.age < b.age) {
-    return -1;
-  } else {
-    0;
-  }
-}
+// function sortAge(a, b) {
+//   if (a.age > b.age) {
+//     return 1;
+//   } else if (a.age < b.age) {
+//     return -1;
+//   } else {
+//     0;
+//   }
+// }
+
+let tableBody = document.querySelector("tbody");
+
+displayData(); // To show the data in UI
+
+// console.log(tableBody);
 
 let ageTd = document.getElementById("age");
 
 ageTd.addEventListener("click", function () {
-  user_data.sort(sortAge);
-  //   window.location.reload();
+  user_data.sort(function (a, b) {
+    return a.age - b.age; // sorting in increasing order
+  });
+  displayData();
 });
 
-user_data.map(function (ele, index, array) {
-  let tbody = document.querySelector("tbody");
+let experienceTd = document.getElementById("experienceTd");
 
-  let tr = document.createElement("tr");
-
-  let sNoTd = document.createElement("td");
-  sNoTd.innerText = ele.id;
-
-  let nameTd = document.createElement("td");
-  nameTd.innerText = ele.name;
-
-  let ageTd = document.createElement("td");
-  ageTd.innerText = ele.age;
-
-  let expTd = document.createElement("td");
-  expTd.innerText = ele.experience;
-
-  let techTd = document.createElement("td");
-  techTd.innerText = ele.tech;
-
-  let favTd = document.createElement("td");
-  favTd.innerText = "❤️";
-
-  tr.append(sNoTd, nameTd, ageTd, expTd, techTd, favTd);
-
-  tbody.append(tr);
+experienceTd.addEventListener("click", function () {
+  user_data.sort(function (a, b) {
+    return a.experience - b.experience; // sorting in increasing order
+  });
+  displayData();
 });
+
+let nameTd = document.getElementById("name");
+
+nameTd.addEventListener("click", function () {
+  user_data.sort(function (a, b) {
+    return a.name - b.name;
+  });
+  displayData();
+});
+
+function displayData() {
+  tableBody.innerHTML = "";
+
+  user_data.map(function (ele, index, array) {
+    let tbody = document.querySelector("tbody");
+
+    let tr = document.createElement("tr");
+
+    let sNoTd = document.createElement("td");
+    sNoTd.innerText = index + 1;
+
+    let nameTd = document.createElement("td");
+    nameTd.innerText = ele.name;
+
+    let ageTd = document.createElement("td");
+    ageTd.innerText = ele.age;
+
+    let expTd = document.createElement("td");
+    expTd.innerText = ele.experience;
+
+    let techTd = document.createElement("td");
+    techTd.innerText = ele.tech;
+
+    let favTd = document.createElement("td");
+    favTd.innerText = "❤️";
+
+    tr.append(sNoTd, nameTd, ageTd, expTd, techTd, favTd);
+
+    tbody.append(tr);
+  });
+}
 
 console.log(user_data);
--- js/user_data.js
+++ js/user_data.js
@@ -1,9 +1,9 @@
 let user_data = [
-  { id: 1, name: "jagdish", age: 23, experience: 1, tech: "js" },
+  { id: 1, name: "jagdish", age: 23, experience: 11, tech: "js" },
   { id: 2, name: "gaurav", age: 24, experience: 2, tech: "html" },
-  { id: 3, name: "shubham", age: 25, experience: 4, tech: "css" },
-  { id: 4, name: "ranjit", age: 27, experience: 5, tech: "react" },
-  { id: 5, name: "mukesh", age: 19, experience: 0, tech: "pursuing" },
+  { id: 3, name: "shubham", age: 25, experience: 14, tech: "css" },
+  { id: 4, name: "ranjit", age: 27, experience: 7, tech: "react" },
+  { id: 5, name: "mukesh", age: 19, experience: 10, tech: "pursuing" },
   { id: 6, name: "ritesh", age: 29, experience: 5, tech: "php" },
   { id: 7, name: "dheeraj", age: 20, experience: 0, tech: "cs graduate" },
   { id: 8, name: "john", age: 29, experience: 7, tech: "MERN" },
